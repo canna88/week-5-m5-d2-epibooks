631e481c90bbfd16cd3a0f64d53891049cf6af32
canna88 <97223496+canna88@users.noreply.github.com>
2023-10-26 13:52:22 +0200

    improvement

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
-import booksData from "./history.json";
+// import booksData from "./history.json";
 import SingleBook from "./SingleBook";
 
 function AllTheBooks() {
+
+
+  const [booksData, setBooksData] = useState([])
+
+  useEffect(() => {
+    fetch("URL")
+
+      .then(response => response.json())
+      .then(fetchData => setBooksData(fetchData))
+      .catch((error) => {
+        console.error("La richiesta non è andata a buon fine")
+      })
+  })
+
+  const (booksToShow, setBooksToShow) = useState([])
+
+  const categoryFilter = (event) => {
+
+    const filteredBooks = booksData.filter(book) => (
+      book.category.toLowerCase().includes(event.target.toLowerCase())
+    )
+
+    setBooksToShow(filteredBooks)
+  }
+
+  // Filtro
   const [filter, setFilter] = useState("");
   const [filteredTitles, setFilteredTitles] = useState([]);
 
@@ -11,42 +37,64 @@ function AllTheBooks() {
     const inputValue = event.target.value;
     setFilter(inputValue);
 
-    const filteredBookTitles = booksData.filter((book) =>
+    const filteredBookTitles = booksToShow.filter((book) =>
       book.title.toLowerCase().includes(inputValue.toLowerCase())
     );
 
     setFilteredTitles(filteredBookTitles);
   };
 
+  // Return
   return (
-    <Container>
-      <Row className="justify-content-center mt-4 my-5">
-        <Col xs={12} md={6} lg={4}>
-          <Form>
-            <Form.Group className="text-center" controlId="filterForm">
-              <Form.Label className="text-center">Filter Books</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter a filter keyword"
-                className="text-center"
-                value={filter}
-                onChange={handleFilterChange}
-              />
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
+    <>
+      <Container>
+        <Tabs
+          defaultActiveKey="profile"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey="History" title="History" onSelect = {categoryFilter}>
+            History
+          </Tab>
+          <Tab eventKey="Scify" title="Scify"onSelect = {categoryFilter}>
+            Scify
+          </Tab>
+          <Tab eventKey="Catastrofy" title="Catastrofy" onSelect = {categoryFilter}>
+            Catastrofy
+          </Tab>
+        </Tabs>
+      </Container>
 
-      <Row>
-        {filteredTitles.length > 0 &&
-          filteredTitles.map((book) => (
-            <SingleBook key={book.asin} book={book} />
-          ))}
-        {filteredTitles.length === 0 &&
-          booksData.map((book) => <SingleBook key={book.asin} book={book} />)}
-      </Row>
-    </Container>
+
+      <Container>
+        <Row className="justify-content-center mt-4 my-5">
+          <Col xs={12} md={6} lg={4}>
+            <Form>
+              <Form.Group className="text-center" controlId="filterForm">
+                <Form.Label className="text-center">Filter Books</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter a filter keyword"
+                  className="text-center"
+                  value={filter}
+                  onChange={handleFilterChange}
+                />
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+
+        <Row>
+          {filteredTitles.length > 0 &&
+            filteredTitles.map((book) => (
+              <SingleBook key={book.asin} book={book} />
+            ))}
+          {filteredTitles.length === 0 &&
+            booksData.map((book) => <SingleBook key={book.asin} book={book} />)}
+        </Row>
+      </Container>
+    </>
   );
-}
 
+}
 export default AllTheBooks;
