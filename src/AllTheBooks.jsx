6e7c0fbd037143f8d64b1f63ba8c64e73f957538
canna88 <97223496+canna88@users.noreply.github.com>
2023-10-26 08:07:47 +0200

    n-commit

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import booksData from "./history.json";
-import Card from "react-bootstrap/Card";
-import { Container, Row, Col } from "react-bootstrap";
+import SingleBook from "./SingleBook";
 
 function AllTheBooks() {
+  const [filter, setFilter] = useState("");
+  const [filteredTitles, setFilteredTitles] = useState([]);
+
+  const handleFilterChange = (event) => {
+    const inputValue = event.target.value;
+    setFilter(inputValue);
+
+    const filteredBookTitles = booksData.filter((book) =>
+      book.title.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
+    setFilteredTitles(filteredBookTitles);
+  };
+
   return (
     <Container>
+      <Row className="justify-content-center mt-4 my-5">
+        <Col xs={12} md={6} lg={4}>
+          <Form>
+            <Form.Group className="text-center" controlId="filterForm">
+              <Form.Label className="text-center">Filter Books</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter a filter keyword"
+                className="text-center"
+                value={filter}
+                onChange={handleFilterChange}
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+
       <Row>
-        {booksData.map((book) => (
-          <Col key={book.asin} xs={12} md={6} lg={4}>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={book.img} alt={book.title} style={{ height: "200px" }} />
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of the card's content.
-                </Card.Text>
-                <p>Price: ${book.price}</p>
-                <p>Category: {book.category}</p>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+        {filteredTitles.length > 0 &&
+          filteredTitles.map((book) => (
+            <SingleBook key={book.asin} book={book} />
+          ))}
+        {filteredTitles.length === 0 &&
+          booksData.map((book) => <SingleBook key={book.asin} book={book} />)}
       </Row>
     </Container>
   );
